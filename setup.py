4ab1749cc392b327ac28a5ee3019bd3924e965f5
JNaeder <J.naeder324@gmail.com>
2023-05-04 12:30:55 -0400

    Init

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='test_package',
+    version='1.0.0',
+    author='John Naeder',
+    author_email='mike_huls@hotmail.com',
+    description='Testing installation of Package',
+    url='https://github.com/mike-huls/toolbox',
+    packages=['test_package'],
+)

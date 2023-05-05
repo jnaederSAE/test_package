dd8f24c994e52096a9b2749ab80bf63d3cb8e916
j.naeder324@gmail.com <j.naeder@sae.edu>
2023-05-05 15:43:41 -0400

    fdjhsk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='test_package',
-    version='1.0.1',
+    version='1.0.2',
     author='John Naeder',
     description='Testing installation of Package',
     url='https://github.com/jnaederSAE/test_package',

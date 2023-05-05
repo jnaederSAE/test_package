83503238842a47e92c6a7539ca9715f609bb69e3
j.naeder324@gmail.com <j.naeder@sae.edu>
2023-05-05 13:28:58 -0400

    More changes

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from test_functions import test_print, add
+
+__all__ = ["test_print", "add"]
--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setuptools.setup(
     name='test_package',
     version='1.0.0',
     author='John Naeder',
-    author_email='mike_huls@hotmail.com',
     description='Testing installation of Package',
     url='https://github.com/jnaederSAE/test_package',
     packages=setuptools.find_packages()

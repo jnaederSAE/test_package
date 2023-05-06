b5f63627a7fd1d71e6a1487b64a2e3c689294e2e
jnaederSAE <j.naeder@sae.edu>
2023-05-05 22:44:33 -0400

    Aded to database init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='test_package',
-    version='1.0.2',
+    version='1.0.3',
     author='John Naeder',
     description='Testing installation of Package',
     url='https://github.com/jnaederSAE/test_package',

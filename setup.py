ed49a5d09cf280abf000567cdcf38a262b2042f7
jnaederSAE <j.naeder@sae.edu>
2023-05-05 22:14:45 -0400

    Tried adding other packages

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setuptools.setup(
     author='John Naeder',
     description='Testing installation of Package',
     url='https://github.com/jnaederSAE/test_package',
+    install_requires=["pymssql==2.2.7",
+                      "python-dotenv==1.0.0"],
     packages=setuptools.find_packages()
 )
